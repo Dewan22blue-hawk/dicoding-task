0c7085d142f7cdb735e106d8c7ad0e2a1684562c
Dewan22blue-hawk <dennyirawan170204@gmail.com>
2024-09-21 14:56:31 +0700

    Adding animation and flexbox

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,48 @@
-// Toggle class active untuk hamburger menu
+// tahun footer
+document.addEventListener("DOMContentLoaded", function () {
+  const currentYearElement = document.getElementById("jsyear");
+  const currentYear = new Date().getFullYear();
+  currentYearElement.textContent = currentYear;
 
-const navbarNav = document.querySelector(".navbar-nav");
+  const textContainer = document.getElementById("text-container");
+  const cursor = document.querySelector(".cursor");
+  const texts = ["Web Developer", "IT Support", "Technical Support", "Cohort Bangkit 2024🌟", "Junior Network Administrator", "Freelancer"];
+  let textIndex = 0;
+  let charIndex = 0;
 
-// ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
-};
+  function type() {
+    textContainer.textContent += texts[textIndex][charIndex];
+    charIndex++;
+    if (charIndex < texts[textIndex].length) {
+      setTimeout(type, 100);
+    } else {
+      setTimeout(erase, 3000);
+    }
+  }
 
-// Klik diluar elemen
+  function erase() {
+    const currentText = texts[textIndex];
+    textContainer.textContent = currentText.substring(0, charIndex);
+    charIndex--;
+    if (charIndex >= 0) {
+      setTimeout(erase, 50);
+    } else {
+      charIndex = 0;
+      textIndex = (textIndex + 1) % texts.length;
+      cursor.style.display = "inline-block";
+      cursorBlink();
+      setTimeout(type, 500);
+    }
+  }
 
-// Update script.js
-const dropdownMenu = document.getElementById("dropdownMenu");
+  function cursorBlink() {
+    let isVisible = true;
+    setInterval(() => {
+      isVisible = !isVisible;
+      cursor.style.opacity = isVisible ? "1" : "0";
+    }, 500);
+  }
 
-document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
-  dropdownMenu.classList.toggle("active"); // Tampilkan dropdown menu
-};
-
-// Menampilkan daftar proyek menggunakan JavaScript
-const projects = ["Proyek E-commerce", "Proyek Landing Page", "Proyek Dashboard Admin", "Proyek Web Portofolio", "Proyek Toko Online"];
-
-const projectList = document.getElementById("projectList");
-
-projects.forEach((project) => {
-  const li = document.createElement("li");
-  li.textContent = project;
-  projectList.appendChild(li);
+  // Memulai animasi pengetikan
+  type();
 });
